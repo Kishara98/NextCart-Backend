660c18a4c523efb418211ae50508b6f8a9f6cbf4
Kishara98 <kisharabackup101@gmail.com>
2025-02-06 23:49:44 +0530

    #01 initial server creation

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.use(express.json());
+const PORT = process.env.PORT;
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('NextCart backend is now running...');
+})
+
