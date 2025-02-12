e02b668738130fac1c39922f886f7a3c294a4ba1
Kishara98 <kisharabackup101@gmail.com>
2025-02-12 14:04:35 +0530

    #05 products get endpoint

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   try {
     const { name, description, imageUrl, unitPrice } = req.body;
-    console.log("Product details: " + req.body);
+    console.log('Product details: ' + req.body);
     res.status(200).json({
       details: {
         name,
@@ -12,11 +12,27 @@ router.post("/", (req, res) => {
         imageUrl,
         unitPrice,
       },
-      message: "Product insert successfully",
+      message: 'Product insert successfully',
     });
   } catch (error) {
-    res.status(200).json({ message: "Product insert error" });
+    res.status(500).json({ message: 'Product insert error' });
   }
 });
 
+router.get('/', (req, res) => {
+  try {
+    const dummyProduct = {
+      name: 'dummy products',
+      description: 'dummy description',
+      imageUrl: 'www.google.com',
+      unitPrice: 25,
+    };
+    res.status(200).json({
+      product: [dummyProduct],
+      message: 'All products fetched',
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Error while fetching products' });
+  }
+});
 module.exports = router;
