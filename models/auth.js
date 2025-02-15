6c84711f7c88299674ad56a364c406d62919039f
Kishara98 <kisharabackup101@gmail.com>
2025-02-15 13:00:25 +0530

    #09 auth endpoints

---
+++ models/auth.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const authSchema = mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+
+const auth = mongoose.model('auth', authSchema);
+
+module.exports = auth;
